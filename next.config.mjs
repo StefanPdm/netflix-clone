0c1cbc68f5a40d61a8ad15fe9309c0badfa33813
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2024-06-06 15:24:17 +0200

    update next.config with new images remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'upload.wikimedia.org', 'uhdtv.io', 'mango.blender.org', 'download.blender.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uhdtv.io',
+        pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'mango.blender.org',
+        pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'download.blender.org',
+        pathname: '/**',
+      },
+    ],
+    // domains: ['avatars.githubusercontent.com', 'upload.wikimedia.org', 'uhdtv.io', 'mango.blender.org', 'download.blender.org'],
   },
 };
 
